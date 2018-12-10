acd3e6dfb8803e994a1c63e22786737a85f53838
Somto Zikora <zikorasomto@gmail.com>
2018-12-10 08:27:09 +0100

    remove onwindowresize and default cube

--- js/billiardSimulation.js
+++ js/billiardSimulation.js
@@ -1,28 +1,24 @@
 const scene = new THREE.Scene();
 const canvas = document.getElementById('canvas');
 const renderer = new THREE.WebGLRenderer({ canvas, antialias: true });
-const camera = new THREE.PerspectiveCamera(75, canvas.width / canvas.height, 0.1, 2000);
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 2000);
 const ambientLight = new THREE.AmbientLight(0x404040);
+renderer.setSize(window.innerWidth, window.innerHeight);
 const controls = new THREE.TrackballControls(camera, canvas);
+const axesHelper = new THREE.AxisHelper(2000);
+
+renderer.setClearColor('white');
 
 camera.position.set(17, 6, 30);
 camera.lookAt(scene.position);
-scene.add(ambientLight);
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
 
-window.addEventListener('resize', onWindowResize, false);
+scene.add(ambientLight);
+scene.add(axesHelper);
 
 function render() {
   requestAnimationFrame(render);
   controls.update();
   renderer.render(scene, camera);
-}
-
-function onWindowResize() {
-  camera.aspect = window.innerWidth / window.innerHeight;
-  camera.updateProjectionMatrix();
-  renderer.setSize(window.innerWidth, window.innerHeight);
 }
 
 render();
