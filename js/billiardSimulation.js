f9379872b29ca5faaa41f81cc2131eecb1c50953
Somto Zikora <zikorasomto@gmail.com>
2018-12-15 12:15:07 +0100

    add table feet

--- js/billiardSimulation.js
+++ js/billiardSimulation.js
@@ -15,10 +15,75 @@ camera.lookAt(scene.position);
 scene.add(ambientLight);
 scene.add(axesHelper);
 
+const floorDimensions = dimensions.floor;
+let floorGeometry = new THREE.PlaneGeometry(floorDimensions.width, floorDimensions.height);
+let floorMaterial = new THREE.MeshLambertMaterial({ color: 0xffff00, side: THREE.DoubleSide });
+let floor = new THREE.Mesh(floorGeometry, floorMaterial);
+
+scene.add(floor);
+orient(floor);
+
+const tableTopDimensions = dimensions.tableTop;
+let tableTop = new THREE.Mesh(
+  new THREE.BoxBufferGeometry(
+    tableTopDimensions.width,
+    tableTopDimensions.height,
+    tableTopDimensions.depth
+  ),
+  new THREE.MeshBasicMaterial({ color: 'green' })
+);
+
+scene.add(tableTop);
+orient(tableTop);
+tableTop.position.y = 11 + tableTopDimensions.depth / 2;
+console.log(tableTop.position);
+
+const tableFootDimensions = dimensions.tableFoot;
+addTableFoot({
+  x: tableTopDimensions.width / 2 - 1,
+  y: tableFootDimensions.depth / 2,
+  z: -tableFootDimensions.depth / 2 - tableTopDimensions.depth / 2,
+});
+addTableFoot({
+  x: tableTopDimensions.width / 2 - 1,
+  y: -tableFootDimensions.depth / 2,
+  z: -tableFootDimensions.depth / 2 - tableTopDimensions.depth / 2,
+});
+addTableFoot({
+  x: -tableTopDimensions.width / 2 + 1,
+  y: tableFootDimensions.depth / 2,
+  z: -tableFootDimensions.depth / 2 - tableTopDimensions.depth / 2,
+});
+addTableFoot({
+  x: -tableTopDimensions.width / 2 + 1,
+  y: -tableFootDimensions.depth / 2,
+  z: -tableFootDimensions.depth / 2 - tableTopDimensions.depth / 2,
+});
+
 function render() {
   requestAnimationFrame(render);
   controls.update();
   renderer.render(scene, camera);
+}
+
+function orient(object) {
+  object.rotateX(-Math.PI / 2);
+}
+
+function addTableFoot({ x = 0, y = 0, z = 0 } = {}) {
+  let tableFoot = new THREE.Mesh(
+    new THREE.BoxBufferGeometry(
+      tableFootDimensions.width,
+      tableFootDimensions.height,
+      tableFootDimensions.depth
+    ),
+    new THREE.MeshBasicMaterial({ color: 'black' })
+  );
+
+  tableTop.add(tableFoot);
+  tableFoot.position.x = x;
+  tableFoot.position.y = y;
+  tableFoot.position.z = z;
 }
 
 render();
---
+++ js/dimensions.js
@@ -0,0 +1,16 @@
+let dimensions = {
+  floor: {
+    height: 50,
+    width: 50,
+  },
+  tableTop: {
+    width: 25.4,
+    height: 12.7,
+    depth: 1,
+  },
+  tableFoot: {
+    width: 2,
+    height: 2,
+    depth: 11,
+  },
+};
